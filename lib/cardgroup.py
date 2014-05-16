7e7e846d46398aa3fbb83cc79e4183f5b32098fd
anabranch <wac.chambers@gmail.com>
2014-05-15 19:38:38 -0700

    Finally am copying cards correctly, the issue was with pointers and copying

--- lib/cardgroup.py
+++ lib/cardgroup.py
@@ -7,42 +7,30 @@ class CardGroup(CommonEqualityMixin):
     """docstring for CardGroup"""
     def __init__(self, cards=[]):
         super(CardGroup, self).__init__()
-        self.cards = cards
+        self._cards = [copy(card) for card in cards]
 
     def __str__(self):
-        return str(self.cards)
+        return str(self._cards)
+
+    def cards(self):
+        return [copy(card) for card in self._cards]
 
     def add_card(self, card):
-        self.cards.append(card)
+        cards = self.cards
+        cards.append(card)
+        self._cards = cards
 
     def get_card(self, number, suit):
-        for c in self.cards:
+        for c in self._cards:
             if c == Card(number, suit):
                 return c
         else: # this should return an error
             return False
 
     def is_flush(self):
-        local_cards = copy(self.cards)
-        suits = [card.suit for card in local_cards]
-        s_counter = Counter(suits)
-        print s_counter
-        print local_cards
-        if max(s_counter.values()) > 4:
+        local_cards = self.cards()
+        suits = Counter([card.suit for card in local_cards])
+        if max(suits.values()) > 4:
             return True
         else:
             return False
-
-    def flush_details(self):
-        local_cards = copy(self.cards)
-        suits = [card.suit for card in local_cards]
-        s_counter = Counter(suits)
-        fl_suit = None
-        for k, v in s_counter.items():
-            if v > 4:
-                fl_suit = k
-        mx = 0
-        for x in local_cards:
-            if mx <= x.number and x.suit == fl_suit:
-                mx = x.number
-        return self.get_card(mx, fl_suit)
