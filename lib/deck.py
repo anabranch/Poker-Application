a1e852df86f64b69a336297fe27892412f05d98b
anabranch <wac.chambers@gmail.com>
2014-05-19 16:18:23 -0700

    Changed our deck to subclass BaseCardGroup

--- lib/deck.py
+++ lib/deck.py
@@ -1,12 +1,12 @@
 from card import Card
+from basecardgroup import BaseCardGroup
 
-class Deck(object):
+class Deck(BaseCardGroup):
     """docstring for Deck"""
     def __init__(self):
         super(Deck, self).__init__()
         suits = ["Diamonds","Clubs","Hearts","Spades"]
         number = range(2,15)
-        self.cards = []
         for suit in suits:
             for card in number:
                 self.cards.append(Card(card, suit))
