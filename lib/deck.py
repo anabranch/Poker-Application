1550d871753729daa207aca1f6d6be41fead9877
anabranch <wac.chambers@gmail.com>
2014-05-19 16:40:10 -0700

    Added Shuffle Method

--- lib/deck.py
+++ lib/deck.py
@@ -1,3 +1,4 @@
+from random import shuffle
 from card import Card
 from baseclasses.basecardgroup import BaseCardGroup
 
@@ -5,8 +6,17 @@ class Deck(BaseCardGroup):
     """docstring for Deck"""
     def __init__(self):
         super(Deck, self).__init__()
+        # potential states :: 'Unshuffled', "Shuffled"
+        self.state = "Unshuffled"
         suits = ["Diamonds","Clubs","Hearts","Spades"]
         number = range(2,15)
         for suit in suits:
             for card in number:
                 self.cards.append(Card(card, suit))
+
+    def _shuffle(self):
+        if self.state != 'Shuffled':
+            self.state = 'Shuffled'
+            shuffle(self.cards)
+        else:
+            print "cards already shuffled"
