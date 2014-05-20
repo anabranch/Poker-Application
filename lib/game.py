fa4fccf62ba91505462fb74a7004fbf0040bbede
anabranch <wac.chambers@gmail.com>
2014-05-19 23:34:13 -0700

    Added pot chips

--- lib/game.py
+++ lib/game.py
@@ -1,11 +1,11 @@
 from baseclasses.statedobject import StatedObject
 from cardgroups import DeckCardGroup, BoardCardGroup, BurnCardGroup
+from chips import PotChips
 
 class PokerGame(StatedObject):
     """docstring for PokerGame"""
     def __init__(self):
         super(PokerGame, self).__init__()
-        # pregame, dealing pocket, dealing, etc
         self.states = [
             "postgame",
             "showdown",
@@ -19,15 +19,24 @@ class PokerGame(StatedObject):
             "dealpocket",
             "pregame"
         ]
-        
-        self.players = []
+
+        self._players = {} # id, player_instance
         # all these card groups should have the same state so we can advance them together
-        self.deck = DeckCardGroup()
-        self.board = BoardCardGroup()
-        self.burn = BurnCardGroup)()
+        self._deck = DeckCardGroup()
+        self._board = BoardCardGroup()
+        self._burn = BurnCardGroup()
+        self._pot = PotChips()
 
         # should have no concept of the current table, it should just get it's own table
         # so people can sit and get up without affecting the game
         
-    def pre_game(self):
+    def pregame(self):
         pass
+
+    def state_change(self):
+        super(PokerGame, self).state_change()
+
+
+# if __name__ == '__main__':
+#     p = PokerGame()
+#     p.state_change()
