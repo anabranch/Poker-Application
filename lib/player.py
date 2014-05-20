843d5536685948df474e6d8cc527e00e63460fa9
anabranch <wac.chambers@gmail.com>
2014-05-19 17:33:38 -0700

    Added player class

---
+++ lib/player.py
@@ -0,0 +1,8 @@
+from card import Card
+
+class Player(object):
+    """docstring for Player"""
+    def __init__(self, arg):
+        super(Player, self).__init__()
+        self.arg = arg
+        
