34783d2b0ef12bce3cb2168a039dee6aa8bf31ad
anabranch <wac.chambers@gmail.com>
2014-05-19 23:26:46 -0700

    Added basic chips methods

--- lib/chips.py
+++ lib/chips.py
@@ -2,9 +2,20 @@ class PlayerChips(object):
     """docstring for PlayerChips"""
     def __init__(self):
         super(PlayerChips, self).__init__()
-        
+        self._stack = 0
+    # need to implement some checks here
+    def commit(self, amount):
+        self._stack = self._stack - amount
+
+    def win(self, amount):
+        self._stack = self._stack + amount
+
+
 class PotChips(object):
     """docstring for PotChips"""
     def __init__(self):
         super(PotChips, self).__init__()
-        self.total = 0
+        self._total = 0
+
+    def commit(self, amount):
+        self.total += amount
