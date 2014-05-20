651bb44ce000828b5d561de531eab6818174f900
anabranch <wac.chambers@gmail.com>
2014-05-19 23:20:03 -0700

    added chips

---
+++ lib/chips.py
@@ -0,0 +1,10 @@
+class PlayerChips(object):
+    """docstring for PlayerChips"""
+    def __init__(self):
+        super(PlayerChips, self).__init__()
+        
+class PotChips(object):
+    """docstring for PotChips"""
+    def __init__(self):
+        super(PotChips, self).__init__()
+        self.total = 0
