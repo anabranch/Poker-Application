e7423eabf7e0dc844f2696e54abc42463fd584e8
anabranch <wac.chambers@gmail.com>
2014-05-21 20:15:23 -0700

    made a player as an object

--- lib/player.py
+++ lib/player.py
@@ -1,14 +1,12 @@
 from card import Card
 from cardgroups import PocketCardGroup
 from chips import PlayerChips
-from baseclasses.statedobject import StatedObject
 
-class Player(StatedObject):
+class Player(object):
     """docstring for Player"""
     def __init__(self, identifier=0, name=None):
         super(Player, self).__init__()
         # States are Initiated, dealing cards, in_game
-        self.state = "Initiated"
         self.name = name
         self.identifier = identifier
         self.pocket = PocketCardGroup()
