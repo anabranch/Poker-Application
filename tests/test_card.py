aaad966759c0610a6834089df2f28588b89ada4b
anabranch <wac.chambers@gmail.com>
2014-05-21 21:10:15 -0700

    Added error tests

--- tests/test_card.py
+++ tests/test_card.py
@@ -8,3 +8,17 @@ class TestCard(object):
     def test_is_ace(self):
         assert self.da.is_ace == True
         assert self.d2.is_ace == False
+
+    def test_wrong_number(self):
+        try:
+            Card(15, "Spades")
+            assert False
+        except TypeError:
+            assert True
+
+    def test_wrong_suit(self):
+        try:
+            Card(13, "sdf")
+            assert False
+        except TypeError:
+            assert True
