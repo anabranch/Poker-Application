cf8bd989c6499a7e58da442fc8a08d8f8def574f
anabranch <wac.chambers@gmail.com>
2014-05-21 21:08:46 -0700

    Added a length requirement to base card group

--- lib/baseclasses/basecardgroups.py
+++ lib/baseclasses/basecardgroups.py
@@ -16,17 +16,24 @@ class BaseCardGroup(CommonEqualityMixin):
         super(BaseCardGroup, self).__init__()
         self.cards = []
         self.cards = cards
+        self.max_cards = 52
 
     def __str__(self):
         return str(self.cards)
 
     def __add__(self, other):
         if isinstance(other, BaseCardGroup):
+            cards = self.local_card_copy()
             for c in other.local_card_copy():
-                self.add_card(c)
-            return self.cards
+                cards.append(c)
+            return cards
+
+    def __len__(self):
+        return len(self.cards)
 
     def add_card(self, card):
+        if len(self.cards) + 1 > self.max_cards:
+            raise ValueError("Cannot Have this many cards")
         self.cards.append(card)
 
     def local_card_copy(self):
