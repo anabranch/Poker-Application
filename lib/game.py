2876f21593ecaf748f829ff0b9e9820af6cd4600
anabranch <wac.chambers@gmail.com>
2014-05-21 21:09:06 -0700

    Added set players and blinds

--- lib/game.py
+++ lib/game.py
@@ -3,10 +3,10 @@ from cardgroups import DeckCardGroup, BoardCardGroup, BurnCardGroup
 from table import Table
 from chips import PotChips
 
-class PokerGame(StatedObject):
-    """docstring for PokerGame"""
+class PokerHand(StatedObject):
+    """docstring for PokerHand"""
     def __init__(self):
-        super(PokerGame, self).__init__()
+        super(PokerHand, self).__init__()
         self.states = [
             "postgame",
             "showdown",
@@ -20,18 +20,47 @@ class PokerGame(StatedObject):
             "dealpocket",
             "pregame"
         ]
-
-        # self._players = {} # id, player_instance # I also think this will be unnecessary
-        self._table = Table()
+        self.button = 0
+        self.positions = dict([(x,None) for x in range(1,13)])
         self._deck = DeckCardGroup()
         self._board = BoardCardGroup()
         self._burn = BurnCardGroup()
         self._pot = PotChips()
 
-        # should have no concept of the current table, it should just get it's own table
-        # so people can sit and get up without affecting the game
+    def _set_players(self, playerposdict):
+        if len(playerposdict) < 2:
+            TypeError("Cannot have a PokerHand with < 2 Players")
+
+    def _set_blinds(self, buttonposition):
+        # this could be a lot easier if we kept track of occupied positions
+        self.button = buttonposition
+        temp = buttonposition
+        if len(self.players) == 2:
+            while True:
+                temp = temp + 1
+                if self.positions[temp]:
+                    self.smallblind = temp
+                    self.bigblind = buttonposition
+                    break
+        else:
+            temp = buttonposition
+            while temp < 12:
+                temp = temp + 1
+                if self.positions[temp]:
+                    self.smallblind = temp
+                    break
+            temp = self.smallblind
+            while temp < 12:
+                temp = temp + 1
+                if self.positions[temp]:
+                    self.bigblind = temp
+                    break
+
         
-    def pregame(self):
+    def pregame_check(self):
+        pass
+
+    def postgame_check(self):
         pass
 
     def state_change(self):
